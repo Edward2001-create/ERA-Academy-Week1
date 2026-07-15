c8a79123672444d8f08b1f63386144f2ac929561
Edward2001-create <leecrafton@outlook.com>
2026-07-14 20:28:52 -0500

    modified server.js 1

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri);
 
-app.get("/", (req, res) => {
-    res.send("backend is running");
-});
+async function startServer() {
+    try {
+        await client.connect();
+        console.log("connected to mongodb");
+        
+        const db = client.db("school_demo");
+        const studentsCollection = db.collection("students");
+        
+        await studentsCollection.insertOne({
+            firstName: "John",
+            lastName: "Doe",
+            gradeLevel: 10
+        });
+        
+        app.get("/", (req, res) => {
+            res.send("backend connected to MongoDB");
+        });
+        
+        app.listen(3000, () => {
+            console.log("server running at http://localhost:3000");
+        });
+    } catch (error) {
+        console.error("error connecting to MongoDB:", error);
+    }
+}
 
-app.listen(3000, () => {
-    console.log("server running at http://localhost:3000");
-});
+startServer();
